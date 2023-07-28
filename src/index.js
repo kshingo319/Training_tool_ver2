fc0359e109ab70b4aad02a27d542543696cda6a5
kshingo319 <130281854+kshingo319@users.noreply.github.com>
2023-07-28 20:23:49 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ datetime.push(copy);
 var daytest = new Date();
 var day_id = daytest.getDay();
 var img_dataset =["image0","image1","image2","image3","image4"];
-var dataset_today = img_dataset[day_id-1];
+var dataset_today = img_dataset[0];
 console.log(dataset_today);
 
 var now = getCurrentTime();
